a48b9e974d2790fdcb04750230f0b0e2e23b0388
kobbyababio <kobbyababio/IODS-porject.git>
2020-11-02 14:22:23 +0200

    Added data with checks

--- data/create_learning2014.R
+++ data/create_learning2014.R
@@ -1,1 +1,8 @@
 #Kwabena Adu-Ababio, 02/11/2020
+
+#Q2: Reading in the data
+lrn14 <- read.table("http://www.helsinki.fi/~kvehkala/JYTmooc/JYTOPKYS3-data.txt", sep="\t", header=TRUE)
+
+#Dimensions and structure of the data file
+dim(lrn14)
+str(lrn14)
