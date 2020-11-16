355d691e8ed49eebac37644b1991c7a19b4a3331
kobbyababio <kobbyababio/IODS-porject.git>
2020-11-16 18:18:56 +0200

    Create script human

---
+++ data/create_human.R
@@ -0,0 +1,7 @@
+#Kwabena Adu-Ababio, 16/11/2020
+# This script extracts and combines two datasets from the UCI Machine Learning Repository 
+# Install and load packages
+library(dplyr)
+library(ggplot2)
+library(GGally)
+library(tidyr)
