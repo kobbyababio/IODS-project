81e713aec212b48cf953e308e39565d93bf0f482
kobbyababio <kobbyababio/IODS-porject.git>
2020-11-29 13:28:38 +0200

    Create new script 6

---
+++ data/meet_and_repeat.R
@@ -0,0 +1,16 @@
+#Kwabena Adu-Ababio, 29/11/2020
+# This script extracts and combines two datasets from the the GitHub repository of MABS
+# Install and load packages
+library(dplyr) 
+library(ggplot2)
+library(GGally)
+library(tidyr)
+library(magrittr)
+
+#Read the “BPRS” and “RATS” data into R
+bprs <- read.table("https://raw.githubusercontent.com/KimmoVehkalahti/MABS/master/Examples/data/BPRS.txt", header = TRUE, sep = '\t')
+
+rats <- read.table("https://raw.githubusercontent.com/KimmoVehkalahti/MABS/master/Examples/data/rats.txt", header = TRUE, sep = '\t')
+
+#Explore the datasets
+
